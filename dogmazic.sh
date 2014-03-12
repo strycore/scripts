f43f8ebf93da3f5d38863a232c723a5431b311f2
Mathieu Comandon <strycore@gmail.com>
2014-03-12 17:13:45 +0100

    dogmazic.sh fixes

--- dogmazic.sh
+++ dogmazic.sh
@@ -4,6 +4,7 @@ set -e
 
 PROJECT_ROOT="/srv/dogmazic"
 BRANCH='dg-next'
+USER=mediagoblin
 
 sudo apt-get install -y git python python-dev python-lxml python-imaging python-virtualenv libxml2-dev libxslt-dev
 sudo apt-get install -y postgresql postgresql-client python-psycopg2
@@ -13,21 +14,25 @@ sudo apt-get install -y python-numpy python-scipy
 sudo apt-get install -y python-dev python-numpy python-setuptools libsndfile-dev
 sudo apt-get install -y libsndfile1-dev libasound2-dev
 
-sudo adduser mediagoblin
+sudo adduser  --system --group --shell /bin/bash $USER
 
+sudo mkdir -p $PROJECT_ROOT
+sudo chown mediagoblin:mediagoblin $PROJECT_ROOT
 cd $PROJECT_ROOT
-git clone https://github.com/MusiqueLibre/DogmaGoblin.git
+sudo -u $USER git clone https://github.com/MusiqueLibre/DogmaGoblin.git
 sudo chown -hR mediagoblin:mediagoblin DogmaGoblin
 
-cd DogmaGoblin
-git checkout $BRANCH
+cd $PROJECT_ROOT/DogmaGoblin
+sudo -u $USER git checkout $BRANCH
 
 sudo -u postgres createuser mediagoblin
 sudo -u postgres createdb -E UNICODE -O mediagoblin mediagoblin
 
-if [ ! -f mediagoblin_local.ini ]; then 
-    cp mediagoblin.ini mediagoblin_local.ini
+if [ ! -f mediagoblin_local.ini ]; then
+    sudo -u $USER cp mediagoblin.ini mediagoblin_local.ini
 fi
 
-(virtualenv --system-site-packages . || virtualenv .)
-./bin/python setup.py develop
+sudo -u $USER virtualenv --system-site-packages .
+sudo -u $USER ./bin/pip install setuptools -U
+sudo -u $USER ./bin/pip install six -U
+sudo -u $USER ./bin/python setup.py develop
