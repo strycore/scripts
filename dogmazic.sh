46945362f689d8a218f47f0b7bc5ce8d71b5ed20
Mathieu Comandon <strycore@gmail.com>
2014-05-05 22:59:34 +0200

    Add missing build dependencies for scipy

--- dogmazic.sh
+++ dogmazic.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y git python python-dev \
 sudo apt-get install -y python-gst0.10 gstreamer0.10-plugins-base \
     gstreamer0.10-plugins-bad gstreamer0.10-plugins-good \
     gstreamer0.10-plugins-ugly gstreamer0.10-ffmpeg \
-    libsndfile1-dev libasound2-dev
+    libsndfile1-dev libasound2-dev libblas-dev liblapack-dev
 
 sudo apt-get install -y python-numpy
 
