987f2c142db4bb020d3d8cbb68d34f46740f4a0d
Mathieu Comandon <strycore@gmail.com>
2014-06-02 19:39:04 +0200

    Fix cp syntax, add /proc/asound listing

--- sound-troubleshoot.sh
+++ sound-troubleshoot.sh
@@ -5,9 +5,10 @@ mkdir -p $DEST
 
 cp /var/log/pm-suspend.log $DEST
 lspci -nn | egrep  'sound|udio' > $DEST/lspci.out
-cp /proc/asound/cards > $DEST/cards
+ls /proc/asound/* > $DEST/asound
+cp /proc/asound/cards $DEST/cards
 pulseaudio --check
 echo "$?" > $DEST/pulse_running
 lsmod > $DEST/modules
 
-tar cvzf ~/sound-info.tar.gz $DEST
+tar czf ~/sound-info.tar.gz $DEST
