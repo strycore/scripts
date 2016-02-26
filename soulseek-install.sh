ffb99e1aeef07eaf5e13f0a74ed079ebcdea571c
Mathieu Comandon <strycore@gmail.com>
2016-02-26 14:38:20 -0800

    Update Soulseek installer again

--- soulseek-install.sh
+++ soulseek-install.sh
@@ -2,20 +2,23 @@
 set -e
 
 arch=$(uname -m)
-version="2015-6-25"
+version="2016-1-17"
+
+if [ ! "$1" ]; then
+    echo "Please specify the path to the latest Soulseek archive downloaded from Dropbox"
+    exit 2
+fi
 
 if [ $arch = 'x86_64' ]; then
-    slsk_url="http://www.soulseekqt.net/SoulseekQT/Linux/SoulseekQt-${version}-64bit.tgz"
     exec_suffix='-64bit'
 else
-    slsk_url="http://www.soulseekqt.net/SoulseekQT/Linux/SoulseekQt-${version}-32bit.tgz"
     exec_suffix=''
 fi
 
 dest_path="/opt/soulseek/"
 sudo mkdir -p $dest_path
 slsk_archive="/tmp/soulseek-client.tar.gz"
-wget $slsk_url -O "${slsk_archive}"
+cp $1 "${slsk_archive}"
 cd $dest_path
 sudo tar xzf ${slsk_archive}
 rm "${slsk_archive}"
